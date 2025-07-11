2e9a90e2804fd779d758659eed00d246c1cae4a9
CAIO FLEMING FERREIRA DE CARVALHO CUNHA <caio_torres@cepel.br>
2025-07-11 18:25:49 -0300

    new bridge protocol

--- new_app.py
+++ new_app.py
@@ -282,7 +282,7 @@ body, html, .stApp {
     position: relative;
     display: block;
     width: 100%;
-    max-width: 290px;
+    max-width: 320px;
     min-width: 200px;
     box-sizing: border-box;
     padding: 2px 20px;
