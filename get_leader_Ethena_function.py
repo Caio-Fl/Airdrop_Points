8b6fd97bc359befd1ddb81ec69a0400cfc08c082
CAIO FLEMING FERREIRA DE CARVALHO CUNHA <caio_torres@cepel.br>
2025-07-27 09:54:36 -0300

    Add markets.json data file

--- get_leader_Ethena_function.py
+++ get_leader_Ethena_function.py
@@ -5,7 +5,9 @@ def get_ethena_Data():
 
     # Definindo os cabeçalhos da requisição
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36',
+        "User-Agent": "Mozilla/5.0",
+        "Referer": "https://app.ethena.fi/leaderboard",
+        "Accept": "*/*"
     }
 
     try:
