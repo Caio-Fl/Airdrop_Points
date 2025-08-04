6deaff2a1a9e44d209b635c36fc95daaba51d5c4
CAIO FLEMING FERREIRA DE CARVALHO CUNHA <caio_torres@cepel.br>
2025-08-04 11:33:00 -0300

    correct fragmetric problem

--- get_rateX_data.py
+++ get_rateX_data.py
@@ -11,8 +11,8 @@ def get_rateX_data(protocol):
             "Accept": "*/*"
     }
     if protocol == "kyros":
-        qS = 37 #kySOL_querySymbol = 15
-        qT = 29 #kySOL_queryTrade = 20
+        qS = 50 #kySOL_querySymbol = 15
+        qT = 40 #kySOL_queryTrade = 20
     else:
         qS = 22 #ragSOL_querySymbol = 21
         qT = 24 #fragSOL_queryTrade = 21
