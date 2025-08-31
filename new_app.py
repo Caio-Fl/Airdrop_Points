e761788c66a21682d47f1d6e1b4e1ede44a6f7d9
CAIO FLEMING FERREIRA DE CARVALHO CUNHA <caio_torres@cepel.br>
2025-08-31 20:54:41 -0300

    add wallet viewer

--- new_app.py
+++ new_app.py
@@ -1892,12 +1892,12 @@ with col_content:
                         display: none; /* Chrome/Safari */
                     }}
                     .container-block {{
-                        width: 390px;
+                        width: 400px;
                         flex-shrink: 0; 
                         margin: 10px;
                     }}
                     .header-wrapper {{
-                        width: 330px;
+                        width: 310px;
                         padding: 30px;
                         margin-top: 10px;
                         margin-right: 15px;
@@ -1950,7 +1950,7 @@ with col_content:
                         text-decoration: none;
                     }}
                     .footer-wrapper {{
-                        width: 330px;
+                        width: 310px;
                         padding: 30px;
                         margin-top: 6px;
                         margin-bottom: 30px;
